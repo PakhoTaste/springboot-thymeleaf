0819b5a6b4ba7407bac2a24c793b2430443bbd3c
liyu <17665204100@163.com>
2019-11-07 23:42:17 +0800

    commit

--- src/main/java/com/liyu/springbootthymeleaf/SpringbootThymeleafApplication.java
+++ src/main/java/com/liyu/springbootthymeleaf/SpringbootThymeleafApplication.java
@@ -1,9 +1,11 @@
 package com.liyu.springbootthymeleaf;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@MapperScan("com.liyu.springbootthymeleaf.mapper")
 public class SpringbootThymeleafApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/liyu/springbootthymeleaf/controller/CategoryController.java
+++ src/main/java/com/liyu/springbootthymeleaf/controller/CategoryController.java
@@ -1,15 +1,65 @@
 package com.liyu.springbootthymeleaf.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.liyu.springbootthymeleaf.entity.Category;
+import com.liyu.springbootthymeleaf.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class CategoryController {
-
+    @Autowired
+    CategoryService categoryService;
     @RequestMapping("/Category")
-    public String CategoryList(){
-        return null;
+    public String CategoryList(Model m,@RequestParam(value = "start",defaultValue = "0") int start,@RequestParam(value = "size",defaultValue = "3")int size) throws  Exception{
+        //在查询前记录需要传到页面的第几页，所以这行代码要在查询之前
+        throw new Exception();
+//        PageHelper.startPage(start,size,"id desc");
+//        List<Category> categorys = categoryService.getAll();
+//        m.addAttribute("cs",categorys);
+//
+//        PageInfo<Category> p = new PageInfo<>(categorys);
+//        m.addAttribute("page",p);
+//        return "CategoryView";
     }
 
+    @RequestMapping("/add")
+    public String add(){
+
+        return "CategoryAdd";
+    }
+
+    @PostMapping("/Category")
+    public String doAdd(Category category){
+        categoryService.add(category);
+        return "redirect:/Category";
+    }
+
+    @RequestMapping("/edit")
+    public String edit(Model m ,String id){
+        Category category = categoryService.getOne(id);
+        m.addAttribute("c",category);
+
+        return "CategoryEdit";
+    }
+
+    @RequestMapping("/update")
+    public String update(Category category){
+        categoryService.update(category);
+
+        return "redirect:/Category";
+    }
+
+    @RequestMapping("/delete")
+    public String delete(String id){
+        categoryService.delete(id);
+
+        return "redirect:/Category";
+    }
 
 }
